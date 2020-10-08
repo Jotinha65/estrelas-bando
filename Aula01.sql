bf36ea50f17b03924facdd8174bc5b25cfb9a502
John Wesley <72558607+Jotinha65@users.noreply.github.com>
2020-10-08 12:42:46 -0300

    Update Aula01.sql

--- Aula01.sql
+++ Aula01.sql
@@ -20,10 +20,11 @@ capital,
 estado,
 renda_per_capta,
 data_fundacao)VALUES 
-('São Paulo', '01311-000',  12.80000, TRUE, 'SP', 1.200, '1554-01-25'),
-('Lomdrina',  '86010-345',   485.000, FALSE, 'PR', 1.200, '1822-09-7'),
-('Rio de Janeiro', '20040-001', 6.320000, TRUE, 'RJ', 1.200, '1556-02-1'),
-('Uberlândia', '38400-128', 584.102,  FALSE,'MG', 1.200, '1888-08-31');
+('São Paulo', '01311-000',  1280000, TRUE, 'SP', 1.200, '1554-01-25'),
+('Lomdrina',  '86010-345',   485000, FALSE, 'PR', 1.200, '1822-09-7'),
+('Rio de Janeiro', '20040-001', 6320000, TRUE, 'RJ', 1.200, '1556-02-1'),
+('Uberlândia', '38400-128', 584.102,  FALSE,'MG', 1.200, '1888-08-31'),
+('Belo Horizonte', '30190-010', 1433000,  TRUE,'MG', 1.200, '1897-12-12');
 
 SELECT * FROM cidade;
 
@@ -35,6 +36,10 @@ SELECT * FROM cidade;
 UPDATE cidade
 SET numero_habitantes = numero_habitantes + (numero_habitantes * 0.10)
 WHERE cep = '01311-000';
+
+UPDATE cidade
+SET numero_habitantes = numero_habitantes + (numero_habitantes * 0.10)
+WHERE cep = '30190-010';
 
 SELECT * FROM cidade;
 
@@ -48,10 +53,11 @@ sigla ) VALUES
 ('São Paulo', 'SP'),
 ('Londrina', 'PR'),
 ('Rio de Janeiro','RJ'),
-('Uberlândia', 'MG');
+('Uberlândia', 'MG'),
+('Belo Horizonte', '');
+
 
 SELECT * FROM ESTADO;
-
 SELECT A.NOME, B.SIGLA FROM
 CIDADE A
 INNER JOIN ESTADO B
@@ -63,7 +69,7 @@ ALTER TABLE cidade
 ADD FOREIGN KEY (estado)
 REFERENCES estado(sigla);
 
-ALTER TABLE estado ADD regiao VARCHAR(100);
+ALTER TABLE estado ADD regiao TEXT(100);
 
 DESCRIBE estado;
 
